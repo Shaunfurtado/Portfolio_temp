1a1d523e5e00fd8a23f63a298b89fe5da3b4e89c
Shaunfurtado <shaunf1801@gmail.com>
2023-09-17 10:03:14 +0530

    Home landing

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Smooth scroll to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 100, // Adjust as needed
+                behavior: 'smooth'
+            });
+        }
+    });
+});
